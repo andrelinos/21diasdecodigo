38275a9fbc7781f7910f15f777ad1187aeb66356
Andrelino Silva <andrelinodev@gmail.com>
2022-08-17 09:17:24 -0300

    Ajustes :apple:

--- readme.markdown
+++ readme.markdown
@@ -3,3 +3,12 @@
 <img src="./public/images/lista-projetos.png" alt="21 Dias de Código" />
 
 ## [Acesse o projeto online](https://21diasdecodigo.vercel.app/)
+
+<br />
+<br />
+<br />
+<br />
+
+---
+
+* _Nota: Pra quem viu que no **Hello World** está faltando um "L", foi de propósito._
--- src/pages/api/data.ts
+++ src/pages/api/data.ts
@@ -4,13 +4,20 @@ export const data = [
         image: '/images/svg-animado.png',
         url: '/helloworld',
         day: '1',
-        techs: ['React JS', 'Typescript', 'CSS', 'Phosphor Icons'],
+        techs: ['React JS', 'Vite JS', 'Typescript', 'CSS', 'Phosphor Icons'],
     },
     {
         title: 'Listagem dos projetos',
         image: '/images/lista-projetos.png',
         url: 'https://github.com/andrelinos/21diasdecodigo',
         day: '2',
-        techs: ['React JS', 'Typescript', 'CSS', 'Phosphor Icons'],
+        techs: [
+            'React JS',
+            'Vite JS',
+            'Typescript',
+            'CSS',
+            'Phosphor Icons',
+            'React Router DOM',
+        ],
     },
 ];
